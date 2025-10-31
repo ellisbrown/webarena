e2dc7db5bfed0fb5a6242b2b00636cc1567a4202
Ellis Brown <ellis.brown@nyu.edu>
2025-10-31 11:15:48 -0700

    fix to_json

--- agent/prompts/to_json.py
+++ agent/prompts/to_json.py
@@ -1,7 +1,15 @@
 import glob
 import importlib
+from pathlib import Path
 import json
 import os
+
+
+prompt_root = Path(__file__).parent
+
+input_dir = prompt_root / "raw"
+output_dir = prompt_root / "jsons"
+output_dir.mkdir(exist_ok=True)
 
 
 # use the current directory as the root
@@ -10,16 +18,21 @@ def run() -> None:
 
     Python files are easiser to edit
     """
-    for p_file in glob.glob(f"agent/prompts/raw/*.py"):
+
+    print(f"Converting python files in {input_dir} to json files in {output_dir}")
+
+    n = 0
+    for p_file in glob.glob(str(input_dir / "*.py")):
         # import the file as a module
-        base_name = os.path.basename(p_file).replace(".py", "")
-        module = importlib.import_module(f"agent.prompts.raw.{base_name}")
+        base_name = os.path.basename(str(p_file)).replace(".py", "")
+        module = importlib.import_module(f"webarena.agent.prompts.raw.{base_name}")
         prompt = module.prompt
         # save the prompt as a json file
-        os.makedirs("agent/prompts/jsons", exist_ok=True)
-        with open(f"agent/prompts/jsons/{base_name}.json", "w+") as f:
+        with open(output_dir / f"{base_name}.json", "w+") as f:
             json.dump(prompt, f, indent=2)
-    print(f"Done convert python files to json")
+        n += 1
+    print(f"Converted {n} python files to json files")
+    print("Done.")
 
 
 if __name__ == "__main__":
